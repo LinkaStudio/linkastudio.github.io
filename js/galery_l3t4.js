95e9ad2595093858386360d868233dd0548c65e4
Ttiki <clem64.combier@gmail.com>
2023-07-25 12:18:30 +0200

    Added new trailer and more func to gallery

--- js/galery_l3t4.js
+++ js/galery_l3t4.js
@@ -1,20 +1,43 @@
+const container = document.getElementById('container');
+
 fetch('/res/T3-L4/galery.json')
     .then(response => response.json())
-    .then(images => {
-        const container = document.getElementById('container');
-        images.forEach(image => {
+    .then(medias => {
+        medias.forEach(media => {
             const div = document.createElement('div');
-            const img = document.createElement('img');
-            img.src = '/res/T3-L4/media/img/galery/' + image.src;
-            img.addEventListener('click', () => {
-                console.log('click');
-                document.getElementById('modal-image').src = img.src;
-                document.getElementById('modal-title').textContent = image.title;
+            let element;
+            if (media.type === 'image') {
+                element = document.createElement('img');
+                element.src = '/res/T3-L4/media/img/galery/' + media.src;
+            } else if (media.type === 'video') {
+                element = document.createElement('iframe');
+                element.src = media.src;
+                element.allow = "autoplay; encrypted-media";
+                element.allowFullscreen = true;
+            }
+            element.addEventListener('click', () => {
+                const modalContent = document.getElementById('modal-content');
+                modalContent.innerHTML = '';  // Clear previous content
+                if (media.type === 'image') {
+                    const img = document.createElement('img');
+                    img.src = element.src;
+                    modalContent.appendChild(img);
+                } else if (media.type === 'video') {
+                    const iframe = document.createElement('iframe');
+                    iframe.src = element.src;
+                    iframe.allow = "autoplay; encrypted-media";
+                    iframe.allowFullscreen = true;
+                    modalContent.appendChild(iframe);
+                }
+                const title = document.createElement('h2');
+                title.textContent = media.title;
+                modalContent.appendChild(title);
                 document.getElementById('modal-preview').showModal();
             });
             const title = document.createElement('p');
-            title.textContent = image.title;
-            div.appendChild(img);
+            title.textContent = media.title;
+            div.classList.add(media.type);  // Add class for filtering
+            div.appendChild(element);
             div.appendChild(title);
             container.appendChild(div);
         });
@@ -23,4 +46,18 @@ fetch('/res/T3-L4/galery.json')
 
 document.getElementById('modal-close').addEventListener('click', () => {
     document.getElementById('modal-preview').close();
+});
+
+// Filtering functionality
+const filters = document.getElementById('filters');
+filters.addEventListener('change', (e) => {
+    const type = e.target.value;
+    const items = container.children;
+    for (let i = 0; i < items.length; i++) {
+        if (type === 'all' || items[i].classList.contains(type)) {
+            items[i].style.display = 'block';
+        } else {
+            items[i].style.display = 'none';
+        }
+    }
 });
